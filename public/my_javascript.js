7d6aabea8d6fda7ca287b6b440c0e213dcdd38ef
Joel <j.n.brewster@gmail.com>
2016-06-08 12:21:29 +1000

    Append to tRow

--- public/my_javascript.js
+++ public/my_javascript.js
@@ -2,15 +2,21 @@ $(function(){
   function getNotes(){
     $.get('/notes', function(data){
       for(var i = 0; i < data.length; i++){
-        var noteDiv = $('<div>').addClass('note').text(data[i].note_text);
-        $('#notes').append(noteDiv);
+        var tRow = $('<tr>');
+        tRow.append(
+          $('<td>').text(data[i].id),
+          $('<td>').text(data[i].note_text),
+          $('<td>').text(data[i].note_category),
+          $('<td>').text('ACTIONS')
+          );
+        $('#notes').append(tRow);
       }
     });
   }
   function createNote(note){
     $.post('/notes', note, function(data){
       var noteDiv = $('<div>').addClass('note').text(data.note_text);
-      $('#notes').append(noteDiv);
+      $('#notes').append(tRow);
     });
   }
   getNotes();

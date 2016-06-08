a100a3eefcdfc0a5ec6758852b6450758a9cf958
Joel <j.n.brewster@gmail.com>
2016-06-08 10:54:51 +1000

    psql define

--- app.rb
+++ app.rb
@@ -1,7 +1,6 @@
 require 'sinatra'
-require 'sinatra'
-require 'sinatra'
 require 'sinatra/reloader'
+require 'active_record'
 
 # NOTES
 # GET /notes
@@ -9,12 +8,17 @@ require 'sinatra/reloader'
 # PATCH /post/:id
 # DELETE /post/:id
 
+# Define a connection
+# Define our model(s)
+# Define database/ table structure
+
 get '/' do
   erb :index
 end
 
 get '/notes' do
-
+  @notes = Note.all
+  return @notes.to_json
 end
 
 post '/notes' do

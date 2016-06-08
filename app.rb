aa891567f4f96f3279be2fe15b86d77a5fd3b69f
Joel <j.n.brewster@gmail.com>
2016-06-08 10:50:22 +1000

    crud end points setup

--- app.rb
+++ app.rb
@@ -3,7 +3,29 @@ require 'sinatra'
 require 'sinatra'
 require 'sinatra/reloader'
 
+# NOTES
+# GET /notes
+# POST /notes
+# PATCH /post/:id
+# DELETE /post/:id
+
 get '/' do
   erb :index
+end
+
+get '/notes' do
+
+end
+
+post '/notes' do
+
+end
+
+patch '/note/:id' do
+
+end
+
+delete '/note/:id' do
+
 end
 

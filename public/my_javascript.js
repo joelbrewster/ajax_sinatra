905321a41fafa7de3f51754237199dbaf3925527
Joel <j.n.brewster@gmail.com>
2016-06-08 13:55:33 +1000

    add edit and delete note

--- public/my_javascript.js
+++ public/my_javascript.js
@@ -1,4 +1,16 @@
 $(function(){
+  function editNote(){
+    console.log($(this).data('note-id'));
+  }
+  function deleteNote(){
+    var noteId = $(this).data('note-id');
+    var del = confirm('Are you sure you want to delete this note?');
+    if(del){
+      $.ajax({
+        url: '/note/'+noteId
+      });
+    }
+  }
   function getNotes(){
     $.get('/notes', function(data){
       for(var i = 0; i < data.length; i++){
@@ -8,31 +20,31 @@ $(function(){
           $('<td>').text(data[i].note_text),
           $('<td>').text(data[i].note_category),
           $('<td>').append(
-            $('<button>').text('Edit'),
-            $('<button>').text('Delete')
+            $('<button>').data('note-id', data[i].id).text('Edit').on('click', editNote),
+            $('<button>').data('note-id', data[i].id).text('Delete').on('click', deleteNote)
             )
+          );
+          $('#notes').append(tRow);
+        }
+      });
+    }
+    function createNote(note){
+      $.post('/notes', note, function(data){
+        var tRow = $('<tr>');
+        tRow.append(
+          $('<td>').text(data.id),
+          $('<td>').text(data.note_text),
+          $('<td>').text(data.note_category),
+          $('<td>').text('ACTIONS')
         );
         $('#notes').append(tRow);
-      }
-    });
-  }
-  function createNote(note){
-    $.post('/notes', note, function(data){
-      var tRow = $('<tr>');
-      tRow.append(
-        $('<td>').text(data.id),
-        $('<td>').text(data.note_text),
-        $('<td>').text(data.note_category),
-        $('<td>').text('ACTIONS')
-      );
-      $('#notes').append(tRow);
-    });
-  }
-  getNotes();
+      });
+    }
+    getNotes();
 
-  $('form').on("submit", function(event){
-    var noteData = $(this).serialize();
-    createNote(noteData);
-    event.preventDefault();
+    $('form').on("submit", function(event){
+      var noteData = $(this).serialize();
+      createNote(noteData);
+      event.preventDefault();
+    });
   });
-});
